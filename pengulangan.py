bc8b512bdb946a675160752cb449cfb9fab3ff89
warsih123 <warsihpaskam123@gmail.com>
2025-05-08 15:34:57 +0700

    tugas pengulangan

---
+++ pengulangan.py
@@ -0,0 +1,12 @@
+for i in range(1, 6):
+    print("Angka ke-", i)
+for i in range(1, 6):
+    print("Angka ke-", i)
+
+for i in range(1, 6):
+    print("Angka ke-", i)
+
+for i in range(1, 4):        # Baris
+    for j in range(1, 4):    # Kolom
+        print(f"{i} x {j} = {i * j}")
+    print("---")  # Pemisah antar baris                                   
