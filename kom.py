640951389cb01b3e1172c575120c517cea550812
warsih123 <warsihpaskam123@gmail.com>
2025-05-08 15:33:51 +0700

    tugas komentar

---
+++ kom.py
@@ -0,0 +1,15 @@
+
+# Menambahkan dua angka
+a = 5
+b = 3
+jumlah = a + b  # Menyimpan hasil penjumlahan
+print("Hasil penjumlahan:", jumlah)
+
+# Fungsi untuk menghitung luas persegi
+def luas_persegi(sisi):
+    # Mengembalikan hasil perhitungan luas
+    return sisi * sisi
+
+# Memanggil fungsi
+sisi = 4
+print("Luas persegi:", luas_persegi(sisi))
