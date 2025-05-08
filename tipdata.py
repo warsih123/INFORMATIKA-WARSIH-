11d0a89c29fa4f0ce9c1f4ee4a37179a32c88ff5
warsih123 <warsihpaskam123@gmail.com>
2025-05-08 15:35:16 +0700

    tugas tipedata

---
+++ tipdata.py
@@ -0,0 +1,33 @@
+# Tipe Data Dasar di Python
+
+# Integer (bilangan bulat)
+angka = 10
+print("Integer:", angka)
+
+# Float (bilangan desimal)
+desimal = 3.14
+print("Float:", desimal)
+
+# String (teks)
+teks = "Halo, Dunia!"
+print("String:", teks)
+
+# Boolean (True/False)
+benar = True
+print("Boolean:", benar)
+
+# List (daftar, bisa berisi banyak jenis data)
+daftar = [1, 2, 3, "empat", 5.0]
+print("List:", daftar)
+
+# Tuple (mirip list, tapi tidak bisa diubah)
+data_tuple = (10, 20, 30)
+print("Tuple:", data_tuple)
+
+# Dictionary (pasangan kunci-nilai)
+data_dict = {"nama": "warsih", "umur": 16}
+print("Dictionary:", data_dict)
+
+# Set (kumpulan nilai unik)
+data_set = {1, 2, 2, 3, 4}
+print("Set:", data_set)
